2542d9c3e33a0b44d22e1c8b8bd90a7a80723d08
Cameron-PC\Cameron <cameronmartincs@gmail.com>
2017-10-05 14:57:12 -0700

    added Towers of Hanoi

---
+++ TowersOfHanoi/TowersOfHanoi/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TowersOfHanoi
+{
+
+    class TowerOfHanoi
+    {
+
+        static void Main(string[] args)
+        {
+
+            int m = 0;
+            Console.Write("Enter the number of discs: ");
+            int numdiscs = Convert.ToInt32(Console.ReadLine());
+            movedisc(numdiscs, 1, 3, 2, ref m);
+            Console.WriteLine("Number of moves = {0}", m);
+
+        }
+        static void movedisc(int n, int begin, int end, int temp, ref int m)
+        {
+            
+            if (n > 0)
+            {
+                movedisc(n - 1, begin, temp, end,ref m);
+                Console.WriteLine("Moved disk {0} from tower {1} to tower {2}", n, begin, end);
+                m++;
+                movedisc(n - 1, temp, end, begin,ref m);
+
+            }
+        }
+    }
+}
